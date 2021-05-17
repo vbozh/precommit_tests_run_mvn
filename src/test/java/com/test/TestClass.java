687efdbbf1d5c63d3f6cd13e845cfd6e0bed6506
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-05-17 17:29:01 +0300

    commit code

---
+++ src/test/java/com/test/TestClass.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class TestClass {
+
+    @Test
+    public void passedMethod() {
+        assertTrue(true);
+    }
+
+}
---
+++ src/test/java/com/test/TestClass2.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestClass2 {
+
+    @Test
+    public void failedMethod() {
+        assertTrue(false);
+    }
+}
